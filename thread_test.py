97ca686daf4a9636f9287c3e9b7d8edbe31e2dae
bogard75 <bogard75@gmail.com>
2017-05-10 00:13:17 +0900

    Add files via upload

--- thread_test.py
+++ thread_test.py
@@ -9,7 +9,7 @@ class Thread_1(threading.Thread):
         run_count = 0
         while True:
             print("[%s] %s" % (++run_count, threading.currentThread().getName()))
-            bs.write_second_history_of(date_list=['20170504'])
+            bs.write_second_history_of(date_list=['20170508'])
             sleep(wait_seconds)
 
 
@@ -20,7 +20,7 @@ class Thread_2(threading.Thread):
             out_file_name = 'out_frgn%s.txt' % strftime("%Y%m%d%H%M%S", localtime())
             print("[%s] %s" % (run_count, threading.currentThread().getName()))
             bs.write_current_top_players(outfile=out_file_name)
-            sleep(30)
+            sleep(10)
             run_count = run_count + 1
 
 
@@ -31,15 +31,17 @@ class Thread_3(threading.Thread):
             out_file_name = 'out_sise%s.txt' % strftime("%Y%m%d%H%M%S", localtime())
             print("[%s] %s" % (run_count, threading.currentThread().getName()))
             bs.write_current_10_quotes(outfile=out_file_name)
-            sleep(30)
+            sleep(10)
             run_count = run_count + 1
 
+#일과 종료 후 한번 실행 (날짜 바꾸고)
 #t1 = Thread_1(name='second_history_of')
-t2 = Thread_2(name='current_top_players')  # 10초 간격으로 상위거래 5사 정보 긁기
-t3 = Thread_3(name='current_10_quotes')    # 10초 간격으로 매도/매수 10호가 정보 긁기
-
 #t1.start()
+
+#일과 중 실시간으로 실행 (Thread, 10초 간격)
+t2 = Thread_2(name='current_top_players')  # 10초 간격으로 상위거래 5사 정보 긁기
 t2.start()
+t3 = Thread_3(name='current_10_quotes')    # 10초 간격으로 매도/매수 10호가 정보 긁기
 t3.start()
 
 while True:
