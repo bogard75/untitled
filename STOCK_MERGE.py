28aee50dce6a42d350bfee5fb9caa6dc008a7ccd
bogard75 <bogard75@gmail.com>
2017-12-26 00:39:07 +0900

    1. Stock-code 입수 (marketdata.krx.co.kr/mdi#document=040402) ☞ STOCK_DATA 2. STOCK_DATA (2,312건) 로 stocks 리스트 생성

---
+++ STOCK_MERGE.py
@@ -0,0 +1,22 @@
+from os import walk
+import pandas as pd
+
+#path = r'C:\Users\taeil\Documents\GitHub\untitled'
+f = []
+for (dirpath, dirnames, filenames) in walk(path):
+    f.extend(filenames)
+    break
+
+def frgn_to_df(filename):
+    df = pd.read_csv(filename,
+                     sep='|',
+                     dtype=object,
+                     names=['입수일시', '종목코드', '매수구분', '거래기관', '거래량', '_'])
+    return df
+
+
+# 201712월 frgn → One File로
+k = [s for s in f if s[:14] == 'out_frgn201712']
+l = pd.concat(map(frgn_to_df, k), ignore_index=True).drop_duplicates(keep='first')
+
+l.to_csv('out_frgn201712.txt', sep='|', index=None, encoding='UTF-8')
